fc8bf83a4f098fe7a30d8b3c9c5343257687c224
Viet Phong <vietphong.dev@gmail.com>
2023-06-08 10:12:37 +0700

    add error handler

---
+++ Error-Handler.ts
@@ -0,0 +1,29 @@
+
+// defined router
+
+// UnKnown Routes
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+  
+// Global Error Handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+  
+const port = config.get<number>('port');
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+  // ? call the connectDB function here
+  connectDB();
+});
+  
